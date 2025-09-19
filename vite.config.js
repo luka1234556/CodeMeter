9fa7ede7482e50fb758744907879e59d3d640be2
Luka Kartvelishvili <lukaqartveli112@gmail.com>
2025-09-19 19:34:16 +0400

    Fix Vite base config for Vercel + GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' && process.env.VERCEL !== '1'
-  ? '/CodeMeter/'
-  : '/', 
+  base: process.env.VERCEL ? "/" : "/CodeMeter/",
   plugins: [
     react(),
     tailwindcss(),
